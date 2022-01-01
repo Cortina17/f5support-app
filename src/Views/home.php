aee59cc0205a3a2242cbbe44c93db87c51fc6311
Cortina17 <diego.cortinagarcia@gmail.com>
2022-01-01 20:36:09 +0100

    Intento de responsividad

--- src/Views/home.php
+++ src/Views/home.php
@@ -1,13 +1,13 @@
-<?php include './src/Views/layouts/header.php'?>
+<?php include './src/Views/layouts/header.php' ?>
 
 <main>
-
-    <section>
+    <div class="row">
+        <div class="col-xs-12 xol-sm-12 col-md-12 col-lg-12 col-xxl-12">
         <section class="d-flex justify-content-center mt-4">
             <a href="?action=create"><button type="button" class="btn btn-info">Añadir petición</button></a>
         </section>
 
-        <table class="table table-striped table-hover mt-3">
+        <table class="table table-striped table-hover mt-3" id="tab-cont">
             <thead>
                 <th>Tema</th>
                 <th>Descripción</th>
@@ -21,28 +21,27 @@
 
                 foreach ($data as $request) {
                     $html = <<<HTML
-                <tr>
-                    <td>{$request->getTopic()}</td>
-                    <td>{$request->getDescription()}</td>
-                    <td>{$request->getUserName()}</td>
-                    <td>{$request->getCreateAt()}</td>
-                    <td class="d-flex justify-content-evenly">
-                        <a href="?action=edit&&id_request={$request->getId()}"><button type="button" class="btn btn-primary">Editar</button></a>
-                        <a href="?action=delete&&id_request={$request->getId()}"><button button type="button" class="btn btn-danger">Borrar</button></a>
-                    </td>
-                </tr>
-                HTML;
+                    <tr>
+                        <td>{$request->getTopic()}</td>
+                        <td>{$request->getDescription()}</td>
+                        <td>{$request->getUserName()}</td>
+                        <td>{$request->getCreateAt()}</td>
+                        <td class="d-flex justify-content-evenly">
+                            <a href="?action=edit&&id_request={$request->getId()}"><button type="button" class="btn btn-primary">Editar</button></a>
+                            <a href="?action=delete&&id_request={$request->getId()}"><button button type="button" class="btn btn-danger">Borrar</button></a>
+                        </td>
+                    </tr>
+                    HTML;
                     echo $html;
                 }
                 ?>
             </tbody>
         </table>
-    </section>
 
-    <section class="d-flex justify-content-center mt-3 mb-3">
-        <a href="index.php" onClick='alert("Ya te encuentras en la página de soporte.")'><button type="button" class="btn btn-warning" id="support">Contactar con soporte</button></a>
-    </section>
-
+        <section class="d-flex justify-content-center mt-3 mb-3">
+            <a href="index.php" onClick='alert("Ya te encuentras en la página de soporte.")'><button type="button" class="btn btn-warning" id="support">Contactar con soporte</button></a>
+        </section>
+    </div>
 </main>
 
 <?php include './src/Views/layouts/footer.php'; ?>
--- src/Views/layouts/footer.php
+++ src/Views/layouts/footer.php
@@ -1,6 +1,6 @@
         <footer class="footer mt-auto py-3 bg-light">
             <div class="container d-flex justify-content-center">
-                <span class="text-muted">Copyright© - Página de soporte F5 - Picateclas 2021</span>
+                <span class="text-muted">Copyright© - Página de soporte F5 - DCWB's 2021</span>
                 </div>
         </footer>
     </body>
